1ac6f347329580e9a790b85398d4f4a03bf99e8d
wu-json <jason.wu2153@gmail.com>
2023-05-07 17:14:42 -0400

    feat: dotenv in dev mode

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,16 @@ use std::env;
 
 #[tokio::main]
 async fn main() {
-    dotenv().ok();
+    let oshi_env: String = env::var("OSHI_ENV").expect("OSHI_ENV is missing");
+    if oshi_env == "development" {
+        println!("Started Oshi-Chan in development mode");
+        dotenv().ok();
+        println!("Loaded environment variables from .env");
+    }
 
     println!("Hello, world!");
-    // let discord_token = env::var("DISCORD_BOT_TOKEN").expect("DISCORD_TOKEN is not defined");
-    // println!("{}", discord_token);
-}
 
+    let discord_token = env::var("DISCORD_BOT_TOKEN").expect("DISCORD_BOT_TOKEN is missing");
+
+    println!("{}", discord_token);
+}
