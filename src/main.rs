231c8ca5cfab6b231ebc4ef17eaf697fa000b35f
wu-json <jason.wu2153@gmail.com>
2023-05-07 17:45:49 -0400

    feat: client creation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 use dotenv::dotenv;
-use serenity::async_trait;
-use serenity::framework::standard::macros::{command, group};
-use serenity::framework::standard::{CommandResult, StandardFramework};
-use serenity::model::channel::Message;
+use serenity::framework::standard::{StandardFramework};
 use serenity::prelude::*;
 use std::env;
 
@@ -21,16 +18,17 @@ async fn main() {
         _ => panic!("OSHI_ENV={oshi_env} is not a valid environment"),
     }
 
-    let framework = StandardFramework::new().configure(|c| c.prefix("~")); // set the bot's prefix to "~"
+    let framework: StandardFramework = StandardFramework::new();
+    let token: String = env::var("DISCORD_BOT_TOKEN").expect("DISCORD_BOT_TOKEN is missing");
+    let intents: GatewayIntents =
+        GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
 
-    let token = env::var("DISCORD_BOT_TOKEN").expect("DISCORD_BOT_TOKEN is missing");
-    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
-    let mut client = Client::builder(&token, intents)
+    let mut client: Client = Client::builder(&token, intents)
         .framework(framework)
         .await
         .expect("Err creating client");
 
     if let Err(why) = client.start().await {
         println!("Client error: {:?}", why);
-    } 
+    }
 }
