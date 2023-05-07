ae50a817a91168975657e9acf319efe059c1611d
wu-json <jason.wu2153@gmail.com>
2023-05-07 16:44:27 -0400

    feat: token

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+use std::env;
+
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
+    let discord_token = env::var("DISCORD_TOKEN").expect("DISCORD_TOKEN is not defined");
+    println!("{}", discord_token);
 }
 
