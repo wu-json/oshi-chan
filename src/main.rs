69dba8ab654982e4e991ee8a4fc55648798201a5
wu-json <jason.wu2153@gmail.com>
2023-05-07 17:09:21 -0400

    feat: dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use dotenv::dotenv;
 use std::env;
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+
     println!("Hello, world!");
-    let discord_token = env::var("DISCORD_TOKEN").expect("DISCORD_TOKEN is not defined");
-    println!("{}", discord_token);
+    // let discord_token = env::var("DISCORD_BOT_TOKEN").expect("DISCORD_TOKEN is not defined");
+    // println!("{}", discord_token);
 }
 
