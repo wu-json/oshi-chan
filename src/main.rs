9e9993b38ac92cb3cafc1337ab408189e1759ee6
wu-json <jason.wu2153@gmail.com>
2023-05-07 17:21:13 -0400

    feat: match

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,18 @@ use std::env;
 #[tokio::main]
 async fn main() {
     let oshi_env: String = env::var("OSHI_ENV").expect("OSHI_ENV is missing");
-    if oshi_env == "development" {
-        println!("Started Oshi-Chan in development mode");
-        dotenv().ok();
-        println!("Loaded environment variables from .env");
+    match oshi_env.as_str() {
+        "development" => {
+            println!("Started Oshi-Chan in development mode");
+            dotenv().ok();
+            println!("Loaded environment variables from .env");
+        }
+        "production" => {
+            println!("Started Oshi-Chan in production mode");
+        }
+        _ => panic!("OSHI_ENV={oshi_env} is not a valid environment"),
     }
 
-    println!("Hello, world!");
-
     let discord_token = env::var("DISCORD_BOT_TOKEN").expect("DISCORD_BOT_TOKEN is missing");
-
     println!("{}", discord_token);
 }
