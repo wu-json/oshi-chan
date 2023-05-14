5f6785590a00daf2a5db43e6692c3c304e0f32d4
wu-json <jason.wu2153@gmail.com>
2023-05-13 23:47:50 -0400

    feat: some scraping improvements

--- scrape-9anime/src/lib.rs
+++ scrape-9anime/src/lib.rs
@@ -1,5 +1,7 @@
 use headless_chrome::Browser;
 use tokio::time::{sleep, Duration};
+use headless_chrome::protocol::cdp::Page;
+
 
 // This code tries to see whether an episode is out by navigating to the
 // url of the episode. 9anime will redirect the client to episode 1 url
@@ -11,11 +13,24 @@ pub async fn is_episode_out(id: &str, episode: u32) -> bool {
     let browser: Browser = Browser::default().unwrap();
     let tab: std::sync::Arc<headless_chrome::Tab> = browser.new_tab().unwrap();
 
+    tab.enable_stealth_mode().unwrap();
+    tab.set_user_agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36", Some("en-US,en;q=0.9,hi;q=0.8,es;q=0.7,lt;q=0.6"), Some("macOS")).unwrap();
+
     tab.navigate_to(&url).unwrap();
 
-    sleep(Duration::from_millis(500)).await;
+    sleep(Duration::from_millis(5000)).await;
+
+    let _jpeg_data = tab.capture_screenshot(
+        Page::CaptureScreenshotFormatOption::Jpeg,
+        None,
+        None,
+        true).unwrap();
+
+    std::fs::write("foo.jpg", _jpeg_data).expect("Failed to write foo.jpg file");
 
     let new_url: String = tab.get_url();
+
+    println!("New url: {}", new_url);
 
     new_url == url
 }
