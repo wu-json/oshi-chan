4569fa46ac585b97fe8704b398ef2cf5adfbbab9
wu-json <jason.wu2153@gmail.com>
2023-05-14 16:53:36 -0400

    feat: connection pooling

--- oshi-chan/src/main.rs
+++ oshi-chan/src/main.rs
@@ -3,8 +3,14 @@ mod environment;
 mod handler;
 
 use environment::{Environment, EnvironmentTrait};
-use pg_client;
+use pg_client::{ConnectionManager, PgConnection, Pool};
 use serenity::{framework::standard::StandardFramework, prelude::*};
+
+pub struct PgPool;
+
+impl serenity::prelude::TypeMapKey for PgPool {
+    type Value = Pool<ConnectionManager<PgConnection>>;
+}
 
 #[tokio::main]
 async fn main() {
@@ -17,8 +23,13 @@ async fn main() {
     let intents: GatewayIntents =
         GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
 
-    let mut connection = pg_client::connect(&Environment::get_database_url());
-    pg_client::migrate(&mut connection);
+    let pool: Pool<ConnectionManager<PgConnection>> =
+        pg_client::create_connection_pool(&Environment::get_database_url());
+    {
+        let connection: &mut pg_client::PooledConnection<ConnectionManager<PgConnection>> =
+            &mut pool.get().unwrap();
+        pg_client::migrate(connection);
+    };
 
     let mut client: Client = Client::builder(&token, intents)
         .event_handler(handler::Handler)
--- pg-client/src/lib.rs
+++ pg-client/src/lib.rs
@@ -1,11 +1,20 @@
 pub mod models;
 pub mod schema;
 
-use diesel::pg::PgConnection;
+pub use diesel::r2d2::{ConnectionManager, Pool, PooledConnection};
+pub use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
 
 pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
+
+pub fn create_connection_pool(database_url: &str)-> Pool<ConnectionManager<PgConnection>> {
+    let manager: ConnectionManager<PgConnection> = ConnectionManager::<PgConnection>::new(database_url);
+    Pool::builder()
+    .test_on_check_out(true)
+    .build(manager)
+    .expect("Failed to build Postgres connection pool")
+}
 
 pub fn connect(database_url: &str) -> PgConnection {
     let connection: PgConnection = PgConnection::establish(database_url).expect(&format!("Error connecting to {}", database_url));
