f3c3fd7091c20ba17ae423f6884205fd920db03b
wu-json <jason.wu2153@gmail.com>
2023-05-14 14:20:22 -0400

    feat: connection

--- pg-client/src/lib.rs
+++ pg-client/src/lib.rs
@@ -2,5 +2,7 @@ use diesel::pg::PgConnection;
 use diesel::prelude::*;
 
 pub fn connect(database_url: &str) -> PgConnection {
-    PgConnection::establish(database_url).expect(&format!("Error connecting to {}", database_url))
+    let connection: PgConnection = PgConnection::establish(database_url).expect(&format!("Error connecting to {}", database_url));
+    println!("Successfully connected to Postgres");
+    connection
 }
