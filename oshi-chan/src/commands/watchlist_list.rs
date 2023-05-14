4efac63b60658730642cff68e56778cd75db84bd
wu-json <jason.wu2153@gmail.com>
2023-05-14 18:25:37 -0400

    feat: list command

--- oshi-chan/src/commands/watchlist_list.rs
+++ oshi-chan/src/commands/watchlist_list.rs
@@ -1,6 +1,6 @@
 use crate::PgPool;
 use pg_client::{models, ConnectionManager, PgConnection, Pool, PooledConnection};
-use serenity::{model::channel::Message, prelude::*};
+use serenity::{model::channel::Message, prelude::*, utils::MessageBuilder};
 
 pub async fn exec(ctx: &Context, msg: &Message) {
     let data: tokio::sync::RwLockReadGuard<TypeMap> = ctx.data.read().await;
@@ -8,4 +8,19 @@ pub async fn exec(ctx: &Context, msg: &Message) {
     let connection: &mut PooledConnection<ConnectionManager<PgConnection>> =
         &mut pool.get().unwrap();
 
+    let results: Vec<models::WatchList> = pg_client::get_watchlist(connection);
+
+    let mut list: String = String::from("");
+    for entry in results {
+        list.push_str(&format!("- {} ({})\n", &entry.name, &entry.nine_anime_id));
+    }
+
+    let content: String = MessageBuilder::new()
+        .push_line("Here are the shows currently on your watchlist!")
+        .push_codeblock(list, Some("bash"))
+        .build();
+
+    if let Err(why) = msg.channel_id.say(&ctx.http, &content).await {
+        println!("watchlist_list: error sending message: {:?}", why);
+    }
 }
