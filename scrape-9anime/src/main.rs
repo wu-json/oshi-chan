fca25ee778515b85ab85d3120cd94a3eed2b6ca5
wu-json <jason.wu2153@gmail.com>
2023-05-13 22:25:43 -0400

    feat: is episode out logic

--- scrape-9anime/src/main.rs
+++ scrape-9anime/src/main.rs
@@ -1,24 +1,5 @@
-use reqwest::{Client};
-
-use headless_chrome::Browser;
-use headless_chrome::protocol::cdp::Page;
-
-fn main() {
-    let url: &str = "https://9anime.to/watch/naruto.xx8z/ep-9291";
-
-    let browser = Browser::default().unwrap();
-    let tab = browser.new_tab().unwrap();
-
-    tab.navigate_to(url).unwrap();
-
-    std::thread::sleep(std::time::Duration::from_millis(500));
-
-    let res = tab.get_url();
-    println!("URL: {}", res);
-
-    let res = tab.get_content().unwrap();
-    // println!("TITLE: {}", res);
-
-    // std::fs::write("results.html", res).unwrap();
-
+#[tokio::main]
+async fn main() {
+   let is_out = scrape_9anime::is_episode_out("mashle-magic-and-muscles.7j2zj", 332).await;
+   println!("IS OUT: {}", is_out);
 }
