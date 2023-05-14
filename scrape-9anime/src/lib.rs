9b518dd20d7e90bc59158200240ef0f271217612
wu-json <jason.wu2153@gmail.com>
2023-05-14 11:05:17 -0400

    feat: more progress

--- scrape-9anime/src/lib.rs
+++ scrape-9anime/src/lib.rs
@@ -1,11 +1,11 @@
 use headless_chrome::Browser;
 use tokio::time::{sleep, Duration};
+use scraper::{Html,Selector};
 
 // This code tries to see whether an episode is out by navigating to the
 // url of the episode. 9anime will redirect the client to episode 1 url
 // if the episode is not out.
 pub async fn is_episode_out(id: &str, episode: u32) -> bool {
-    // example: "https://9anime.to/watch/naruto.xx8z/ep-13"
     let url: String = format!("https://9anime.to/watch/{id}/ep-{episode}");
 
     let browser: Browser = Browser::default().unwrap();
@@ -16,9 +16,51 @@ pub async fn is_episode_out(id: &str, episode: u32) -> bool {
 
     tab.navigate_to(&url).unwrap();
 
-    sleep(Duration::from_millis(5000)).await;
+    sleep(Duration::from_millis(3000)).await;
 
     let new_url: String = tab.get_url();
 
     new_url == url
+}
+
+#[derive(Debug)]
+pub struct Anime {
+    pub id: String,
+    pub name: String,
+    pub description: String,
+    pub poster_img_url: String,
+    pub total_episodes: u32,
+}
+
+pub async fn scrape_anime(id: &str) -> Anime {
+    let url: String = format!("https://9anime.to/watch/{id}");
+
+    let browser: Browser = Browser::default().unwrap();
+    let tab: std::sync::Arc<headless_chrome::Tab> = browser.new_tab().unwrap();
+
+    tab.enable_stealth_mode().unwrap();
+    tab.set_user_agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36", Some("en-US,en;q=0.9,hi;q=0.8,es;q=0.7,lt;q=0.6"), Some("macOS")).unwrap();
+
+    tab.navigate_to(&url).unwrap();
+
+    sleep(Duration::from_millis(3000)).await;
+
+    let content: String = tab.get_content().unwrap();
+    let document: Html = Html::parse_document(&content);
+    let name_selector:Selector = Selector::parse("div.info h1.title").unwrap();
+
+    let name_elem = document.select(&name_selector).next().unwrap();
+    let name: Vec<&str> = name_elem.text().collect::<Vec<_>>();
+
+
+
+
+
+    Anime {
+        id: String::from(""),
+        name: String::from(name[0]),
+        description: String::from(""),
+        poster_img_url: String::from(""),
+        total_episodes: 23
+    }
 }
--- scrape-9anime/src/main.rs
+++ scrape-9anime/src/main.rs
@@ -1,5 +1,8 @@
 #[tokio::main]
 async fn main() {
-   let is_out: bool = scrape_9anime::is_episode_out("mashle-magic-and-muscles.7j2zj", 81).await;
-   println!("IS OUT: {}", is_out);
+   // let is_out: bool = scrape_9anime::is_episode_out("mashle-magic-and-muscles.7j2zj", 81).await;
+   // println!("IS OUT: {}", is_out);
+
+   let res = scrape_9anime::scrape_anime("mashle-magic-and-muscles.7j2zj").await;
+   println!("RESULT: {:?}", res);
 }
