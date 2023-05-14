87fc114d6565f3bd5c7e04f906c6f00710441d7c
wu-json <jason.wu2153@gmail.com>
2023-05-14 13:06:18 -0400

    feat: migrations

--- oshi-chan/migrations/2023-05-14-030102_create_watchlist/up.sql
+++ oshi-chan/migrations/2023-05-14-030102_create_watchlist/up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE watchlist {
+CREATE TABLE watchlist (
     id SERIAL PRIMARY KEY,
     name VARCHAR NOT NULL,
     description TEXT NOT NULL,
@@ -7,7 +7,7 @@ CREATE TABLE watchlist {
     latest_episode INT NOT NULL,
     total_episodes INT NOT NULL,
     created_on TIMESTAMP NOT NULL,
-}
+);
 
 CREATE INDEX ix_watchlist_nine_anime_id ON watchlist(nine_anime_id);
 CREATE INDEX ix_watchlist_latest_episode ON watchlist(latest_episode);
