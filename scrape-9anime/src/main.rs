9fc66496c62ad06207424435a6a3ab1643b89006
wu-json <jason.wu2153@gmail.com>
2023-05-13 21:33:04 -0400

    feat: some scraping progress

--- scrape-9anime/src/main.rs
+++ scrape-9anime/src/main.rs
@@ -1,3 +1,22 @@
+use reqwest::{Client};
+
+use headless_chrome::Browser;
+use headless_chrome::protocol::cdp::Page;
+
 fn main() {
-    println!("Look: {}", scrape_9anime::add(6, 2));
+    let url: &str = "https://9anime.to/watch/mashle-magic-and-muscles.7j2zj/ep-100";
+
+    let browser = Browser::default().unwrap();
+    let tab = browser.new_tab().unwrap();
+
+    tab.navigate_to(url).unwrap();
+
+    std::thread::sleep(std::time::Duration::from_millis(500));
+
+    let res = tab.get_url();
+    println!("URL: {}", res);
+
+    let res = tab.get_content().unwrap();
+    println!("TITLE: {}", res);
+
 }
