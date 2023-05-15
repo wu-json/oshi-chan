eea6102ffb131cf240bfbcc597e7f2883af2b5b8
Jason Wu <45532884+wu-json@users.noreply.github.com>
2023-05-14 20:43:39 -0400

    feat: delete command (#10)

--- pg-client/src/lib.rs
+++ pg-client/src/lib.rs
@@ -3,7 +3,8 @@ pub mod schema;
 
 pub use diesel::pg::PgConnection;
 pub use diesel::r2d2::{ConnectionManager, Pool, PooledConnection};
-use diesel::RunQueryDsl;
+use diesel::ExpressionMethods;
+use diesel::{QueryDsl, RunQueryDsl};
 use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
 use models::{NewWatchListEntry, WatchList};
 use schema::watchlist::dsl::*;
@@ -37,4 +38,10 @@ pub fn get_watchlist(connection: &mut PgConnection) -> Vec<WatchList> {
     watchlist
         .load::<WatchList>(connection)
         .expect("Error loading watchlist")
+}
+
+pub fn delete_watchlist_entry(connection: &mut PgConnection, anime_id: &str) {
+    diesel::delete(watchlist.filter(nine_anime_id.eq(anime_id)))
+        .execute(connection)
+        .unwrap();
 }
