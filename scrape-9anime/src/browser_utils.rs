4f0265fedd010e0c1c862b5fad54500fa8e3ede6
Jason Wu <45532884+wu-json@users.noreply.github.com>
2023-05-29 15:36:58 -0400

    feat: cleanup scraper (#12)

---
+++ scrape-9anime/src/browser_utils.rs
@@ -0,0 +1,43 @@
+use headless_chrome::Browser;
+use std::sync::Arc;
+use thiserror::Error;
+
+pub struct BrowserUtils {}
+pub trait TabUtils {
+    fn create_browser_tab() -> Result<(Browser, Arc<headless_chrome::Tab>), CreateBrowserTabError>;
+}
+
+#[derive(Error, Debug)]
+pub enum CreateBrowserTabError {
+    #[error("Browser create error.")]
+    CreateError(String),
+    #[error("Browser new tab error.")]
+    NewTabError(String),
+    #[error("Stealth mode error.")]
+    StealthModeError(String),
+    #[error("Set user agent error.")]
+    SetUserAgentError(String),
+}
+
+const USER_AGENT: &str = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36";
+const ACCEPT_LANGUAGE: &str = "en-US,en;q=0.9,hi;q=0.8,es;q=0.7,lt;q=0.6";
+const PLATFORM: &str = "macOS";
+
+impl TabUtils for BrowserUtils {
+    fn create_browser_tab() -> Result<(Browser, Arc<headless_chrome::Tab>), CreateBrowserTabError> {
+        let browser =
+            Browser::default().map_err(|e| CreateBrowserTabError::CreateError(e.to_string()))?;
+
+        let tab = browser
+            .new_tab()
+            .map_err(|e| CreateBrowserTabError::NewTabError(e.to_string()))?;
+
+        tab.enable_stealth_mode()
+            .map_err(|e| CreateBrowserTabError::StealthModeError(e.to_string()))?;
+
+        tab.set_user_agent(USER_AGENT, Some(ACCEPT_LANGUAGE), Some(PLATFORM))
+            .map_err(|e| CreateBrowserTabError::SetUserAgentError(e.to_string()))?;
+
+        return Ok((browser, tab));
+    }
+}
--- scrape-9anime/src/lib.rs
+++ scrape-9anime/src/lib.rs
@@ -1,59 +1,34 @@
-use headless_chrome::Browser;
+mod browser_utils;
+use browser_utils::{BrowserUtils, TabUtils};
 use scraper::{Html, Selector};
 use thiserror::Error;
 use tokio::time::{sleep, Duration};
 
 #[derive(Error, Debug)]
-
 pub enum IsEpisodeOutError {
-    #[error("Browser error.")]
-    BrowserError(String),
-    #[error("Browser tab error.")]
-    BrowserTabError(String),
-    #[error("Stealth mode error.")]
-    StealthModeError(String),
-    #[error("Set user agent error.")]
-    SetUserAgentError(String),
+    #[error("Create browser tab error.")]
+    CreateBrowserTabError(browser_utils::CreateBrowserTabError),
+    #[error("Tab navigate error.")]
+    TabNavigateError(String),
 }
 
-// This code tries to see whether an episode is out by navigating to the
-// url of the episode. 9anime will redirect the client to episode 1 url
-// if the episode is not out.
+/// Checks whether an episode is out by navigating to the url of the episode.
+/// 9anime will redirect the client to episode 1 url if the episode is not out,
+/// otherwise it will render the episode page.
 pub async fn is_episode_out(id: &str, episode: u32) -> Result<bool, IsEpisodeOutError> {
-    let url: String = format!("https://9anime.to/watch/{id}/ep-{episode}");
+    let url = format!("https://9anime.to/watch/{id}/ep-{episode}");
+    let (_browser, tab) = BrowserUtils::create_browser_tab()
+        .map_err(|e| IsEpisodeOutError::CreateBrowserTabError(e))?;
 
-    let browser: Browser = match Browser::default() {
-        Ok(b) => b,
-        Err(e) => return Err(IsEpisodeOutError::BrowserError(e.to_string())),
-    };
+    tab.navigate_to(&url)
+        .map_err(|e| IsEpisodeOutError::TabNavigateError(e.to_string()))?
+        .wait_until_navigated()
+        .map_err(|e| IsEpisodeOutError::TabNavigateError(e.to_string()))?;
 
-    let tab: std::sync::Arc<headless_chrome::Tab> = match browser.new_tab() {
-        Ok(t) => t,
-        Err(e) => return Err(IsEpisodeOutError::BrowserTabError(e.to_string())),
-    };
+    // small buffer to be extra sure redirect occurred if episode is not out
+    sleep(Duration::from_millis(1000)).await;
 
-    match tab.enable_stealth_mode() {
-        Ok(_) => {}
-        Err(e) => return Err(IsEpisodeOutError::StealthModeError(e.to_string())),
-    };
-
-    match tab.set_user_agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36", Some("en-US,en;q=0.9,hi;q=0.8,es;q=0.7,lt;q=0.6"), Some("macOS")) {
-        Ok(_) => {},
-        Err(e) => return Err(IsEpisodeOutError::SetUserAgentError(e.to_string()))
-    };
-
-    match tab.navigate_to(&url) {
-        Ok(_) => {}
-        Err(e) => return Err(IsEpisodeOutError::BrowserTabError(e.to_string())),
-    };
-
-    // wait longer here since we aren't in a huge rush and want to make sure
-    // the result is accurate
-    sleep(Duration::from_millis(10000)).await;
-
-    let new_url: String = tab.get_url();
-
-    Ok(new_url == url)
+    Ok(tab.get_url() == url)
 }
 
 #[derive(Debug)]
@@ -68,14 +43,10 @@ pub struct Anime {
 #[derive(Error, Debug)]
 
 pub enum ScrapeAnimeError {
-    #[error("Browser error.")]
-    BrowserError(String),
-    #[error("Browser tab error.")]
-    BrowserTabError(String),
-    #[error("Stealth mode error.")]
-    StealthModeError(String),
-    #[error("Set user agent error.")]
-    SetUserAgentError(String),
+    #[error("Create browser tab error.")]
+    CreateBrowserTabError(browser_utils::CreateBrowserTabError),
+    #[error("Tab navigate error.")]
+    TabNavigateError(String),
     #[error("Content retrieval error.")]
     ContentRetrievalError(String),
     #[error("Selector creation error.")]
@@ -85,110 +56,78 @@ pub enum ScrapeAnimeError {
 }
 
 pub async fn scrape_anime(id: &str) -> Result<Anime, ScrapeAnimeError> {
-    let url: String = format!("https://9anime.to/watch/{id}");
+    let url = format!("https://9anime.to/watch/{id}");
+    let (_browser, tab) = BrowserUtils::create_browser_tab()
+        .map_err(|e| ScrapeAnimeError::CreateBrowserTabError(e))?;
 
-    let browser: Browser = match Browser::default() {
-        Ok(b) => b,
-        Err(e) => return Err(ScrapeAnimeError::BrowserError(e.to_string())),
-    };
+    tab.navigate_to(&url)
+        .map_err(|e| ScrapeAnimeError::TabNavigateError(e.to_string()))?
+        .wait_until_navigated()
+        .map_err(|e| ScrapeAnimeError::TabNavigateError(e.to_string()))?;
 
-    let tab: std::sync::Arc<headless_chrome::Tab> = match browser.new_tab() {
-        Ok(t) => t,
-        Err(e) => return Err(ScrapeAnimeError::BrowserTabError(e.to_string())),
-    };
+    // small buffer to make sure page loaded
+    sleep(Duration::from_millis(1000)).await;
 
-    match tab.enable_stealth_mode() {
-        Ok(_) => {}
-        Err(e) => return Err(ScrapeAnimeError::StealthModeError(e.to_string())),
-    };
+    let content = tab
+        .get_content()
+        .map_err(|e| ScrapeAnimeError::ContentRetrievalError(e.to_string()))?;
 
-    match tab.set_user_agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36", Some("en-US,en;q=0.9,hi;q=0.8,es;q=0.7,lt;q=0.6"), Some("macOS")) {
-        Ok(_) => {},
-        Err(e) => return Err(ScrapeAnimeError::SetUserAgentError(e.to_string()))
-    };
+    let document = Html::parse_document(&content);
 
-    match tab.navigate_to(&url) {
-        Ok(_) => {}
-        Err(e) => return Err(ScrapeAnimeError::BrowserTabError(e.to_string())),
-    };
+    let name = Selector::parse("div.info h1.title")
+        .map_err(|e| ScrapeAnimeError::SelectorCreationError(e.to_string()))?;
+    let name = document
+        .select(&name)
+        .next()
+        .ok_or(ScrapeAnimeError::SelectorNotFoundError(String::from(
+            "Name not found.",
+        )))?;
+    let name = name.text().collect::<Vec<_>>()[0];
 
-    sleep(Duration::from_millis(5000)).await;
+    let desc = Selector::parse("div.info div.shorting div.content")
+        .map_err(|e| ScrapeAnimeError::SelectorCreationError(e.to_string()))?;
+    let desc = document
+        .select(&desc)
+        .next()
+        .ok_or(ScrapeAnimeError::SelectorNotFoundError(String::from(
+            "Description not found.",
+        )))?;
+    let desc = desc.text().collect::<Vec<_>>()[0];
 
-    let content: String = match tab.get_content() {
-        Ok(c) => c,
-        Err(e) => return Err(ScrapeAnimeError::ContentRetrievalError(e.to_string())),
-    };
-
-    let document: Html = Html::parse_document(&content);
-
-    let name: Selector = match Selector::parse("div.info h1.title") {
-        Ok(s) => s,
-        Err(e) => return Err(ScrapeAnimeError::SelectorCreationError(e.to_string())),
-    };
-    let name: scraper::ElementRef = match document.select(&name).next() {
-        Some(s) => s,
-        None => {
-            return Err(ScrapeAnimeError::SelectorNotFoundError(String::from(
-                "Name not found.",
-            )))
-        }
-    };
-    let name: Vec<&str> = name.text().collect::<Vec<_>>();
-    let name: &str = name[0];
-
-    let desc: Selector = match Selector::parse("div.info div.shorting div.content") {
-        Ok(s) => s,
-        Err(e) => return Err(ScrapeAnimeError::SelectorCreationError(e.to_string())),
-    };
-    let desc: scraper::ElementRef = match document.select(&desc).next() {
-        Some(s) => s,
-        None => {
-            return Err(ScrapeAnimeError::SelectorNotFoundError(String::from(
-                "Description not found.",
-            )))
-        }
-    };
-    let desc: Vec<&str> = desc.text().collect::<Vec<_>>();
-    let desc: &str = desc[0];
-
-    let poster_img: Selector = match Selector::parse("div.binfo div.poster span img") {
-        Ok(s) => s,
-        Err(e) => return Err(ScrapeAnimeError::SelectorCreationError(e.to_string())),
-    };
-    let poster_img: scraper::ElementRef = match document.select(&poster_img).next() {
-        Some(s) => s,
-        None => {
-            return Err(ScrapeAnimeError::SelectorNotFoundError(String::from(
+    let poster_img = Selector::parse("div.binfo div.poster span img")
+        .map_err(|e| ScrapeAnimeError::SelectorCreationError(e.to_string()))?;
+    let poster_img =
+        document
+            .select(&poster_img)
+            .next()
+            .ok_or(ScrapeAnimeError::SelectorNotFoundError(String::from(
                 "Poster image not found.",
-            )))
-        }
-    };
-    let poster_img: &str = match poster_img.value().attr("src") {
-        Some(s) => s,
-        None => {
-            return Err(ScrapeAnimeError::SelectorNotFoundError(String::from(
+            )))?;
+    let poster_img =
+        poster_img
+            .value()
+            .attr("src")
+            .ok_or(ScrapeAnimeError::SelectorNotFoundError(String::from(
                 "Poster image src attribute not found.",
-            )))
-        }
-    };
+            )))?;
 
-    let total_episodes: Selector = match Selector::parse("div.info div.bmeta div.meta div") {
-        Ok(s) => s,
-        Err(e) => return Err(ScrapeAnimeError::SelectorCreationError(e.to_string())),
-    };
-    let total_episodes: Vec<scraper::ElementRef> =
-        document.select(&&total_episodes).collect::<Vec<_>>();
+    let total_episodes = Selector::parse("div.info div.bmeta div.meta div")
+        .map_err(|e| ScrapeAnimeError::SelectorCreationError(e.to_string()))?;
+    let total_episodes = document.select(&&total_episodes).collect::<Vec<_>>();
     let mut total_episodes_count: u32 = 12;
 
     for ep in total_episodes {
-        let text: Vec<&str> = ep.text().collect::<Vec<_>>();
+        let text = ep.text().collect::<Vec<_>>();
         if text[0] == "Episodes: " {
             total_episodes_count = match text[1].parse::<u32>() {
                 Ok(c) => c,
                 Err(_) => {
-                    println!("Error parsing total episode count. Falling back on default ({}).", total_episodes_count);
+                    println!(
+                        "Error parsing total episode count. Falling back on default ({}).",
+                        total_episodes_count
+                    );
                     total_episodes_count
-                },
+                }
             };
             break;
         }
