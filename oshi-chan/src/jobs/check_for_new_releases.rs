c00b0127d8d1b0e8ad47b470bd7a9d9b2d55a355
wu-json <jason.wu2153@gmail.com>
2023-07-11 01:53:26 -0400

    test

--- oshi-chan/src/jobs/check_for_new_releases.rs
+++ oshi-chan/src/jobs/check_for_new_releases.rs
@@ -52,46 +52,46 @@ impl OshiJob for CheckForNewReleasesJob {
                 }
             );
 
-            if new_episode_out {
-                pg_client::update_watchlist_entry(
-                    connection,
-                    &anime.nine_anime_id,
-                    new_episode as i32,
-                );
-                new_releases.push(anime);
-            }
+            // if new_episode_out {
+            //     pg_client::update_watchlist_entry(
+            //         connection,
+            //         &anime.nine_anime_id,
+            //         new_episode as i32,
+            //     );
+            //     new_releases.push(anime);
+            // }
         }
 
         println!("Found {} new releases", new_releases.len());
 
-        let channel_id = Environment::get_oshi_general_channel_id();
-        for anime in new_releases {
-            let mut message = CreateMessage::default();
-            message.embed(|e| {
-                e.colour(0x800080)
-                    .thumbnail(anime.post_img_url)
-                    .title(format!(
-                        "\"{}\" episode {} is out!",
-                        &anime.name,
-                        anime.latest_episode + 1
-                    ))
-                    .description(format!(
-                        "Check it out at https://9anime.to/watch/{}/ep-{}",
-                        anime.nine_anime_id,
-                        anime.latest_episode + 1
-                    ))
-            });
+        // let channel_id = Environment::get_oshi_general_channel_id();
+        // for anime in new_releases {
+        //     let mut message = CreateMessage::default();
+        //     message.embed(|e| {
+        //         e.colour(0x800080)
+        //             .thumbnail(anime.post_img_url)
+        //             .title(format!(
+        //                 "\"{}\" episode {} is out!",
+        //                 &anime.name,
+        //                 anime.latest_episode + 1
+        //             ))
+        //             .description(format!(
+        //                 "Check it out at https://9anime.to/watch/{}/ep-{}",
+        //                 anime.nine_anime_id,
+        //                 anime.latest_episode + 1
+        //             ))
+        //     });
 
-            if let Err(why) = channel_id
-                .send_message(&http, |m| {
-                    *m = message;
-                    m
-                })
-                .await
-            {
-                println!("check_for_new_releases: error sending message: {:?}", why);
-            }
-        }
+        //     if let Err(why) = channel_id
+        //         .send_message(&http, |m| {
+        //             *m = message;
+        //             m
+        //         })
+        //         .await
+        //     {
+        //         println!("check_for_new_releases: error sending message: {:?}", why);
+        //     }
+        // }
     }
 
     fn make_job(http: Arc<Http>, pool: Pool<ConnectionManager<PgConnection>>) -> Job {
